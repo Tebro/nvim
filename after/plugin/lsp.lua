cfec8f9edc6022c1ba78fa03693e13c95cd4574b
Richard Weber <git@tebro.simplelogin.com>
2024-06-29 14:53:13 +0300

    LSP Restart shortcut

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,6 +7,7 @@ lsp_zero.on_attach(function(client, bufnr)
 
   local opts = { buffer = bufnr, remap = false }
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+	vim.keymap.set('n', '<leader>lr', ':LspRestart<cr>', opts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
   vim.keymap.set('n', '<leader>lf', function()
     vim.lsp.buf.format { async = true }
