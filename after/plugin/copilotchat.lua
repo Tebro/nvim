3dfc9bf4e0058cd146de8c5eeec39ef0e8e1a27e
Richard Weber <git@tebro.simplelogin.com>
2024-08-16 12:13:08 +0300

    Add Copilot chat commands

---
+++ after/plugin/copilotchat.lua
@@ -0,0 +1,6 @@
+
+vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatToggle<CR>')
+vim.keymap.set('n', '<leader>c<return>', ':CopilotChat ')
+vim.keymap.set('v', '<leader>c<return>', ':CopilotChat ')
+vim.keymap.set('n', '<leader>crr', '<cmd>CopilotChatReview<CR>')
+vim.keymap.set('n', '<leader>crc', '<cmd>CopilotChatReviewClear<CR>')
