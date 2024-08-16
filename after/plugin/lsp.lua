71ad93c440639b3bdf2932689a9eb3bc999c885f
Richard Weber <git@tebro.simplelogin.com>
2024-08-16 11:40:12 +0300

    Add go to definition bind for LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,32 +1,33 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 
-  local opts = { buffer = bufnr, remap = false }
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+	local opts = { buffer = bufnr, remap = false }
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
 	vim.keymap.set('n', '<leader>lr', ':LspRestart<cr>', opts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>lf', function()
-    vim.lsp.buf.format { async = true }
-  end, opts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', '<leader>lf', function()
+		vim.lsp.buf.format { async = true }
+	end, opts)
 end)
 
 
 require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
 })
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver', 'rust_analyzer', 'gopls' },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+	ensure_installed = { 'tsserver', 'rust_analyzer', 'gopls' },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
