169a3fb8355979222608d85ab425967f7fc88d48
Richard Weber <git@tebro.simplelogin.com>
2025-01-08 14:58:23 +0200

    Replace avante with codecompanion

--- after/plugin/copilotchat.lua
+++ after/plugin/copilotchat.lua
@@ -4,3 +4,12 @@ vim.keymap.set('n', '<leader>c<return>', ':CopilotChat ')
 vim.keymap.set('v', '<leader>c<return>', ':CopilotChat ')
 vim.keymap.set('n', '<leader>crr', '<cmd>CopilotChatReview<CR>')
 vim.keymap.set('n', '<leader>crc', '<cmd>CopilotChatReviewClear<CR>')
+
+vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,26 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function ()
+		require("codecompanion").setup({
+			adapters = {
+				copilot = function ()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								default = "claude-3.5-sonnet"
+							}
+						}
+					})
+				end
+			},
+			strategies = {
+				chat = { adapter = "copilot" },
+				inline = { adapter = "copilot" },
+			}
+		})
+  end
+}
