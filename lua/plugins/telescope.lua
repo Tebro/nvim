1a5a2be7f5999c90ebfb0ba4a6938b39cb678d96
Richard Weber <git@tebro.simplelogin.com>
2025-01-09 16:24:19 +0200

    Find symbol

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,8 @@ return { { -- Fuzzy file finder, also does buffers and live grep
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fG', builtin.grep_string, {})
+    vim.keymap.set('v', '<leader>fG', builtin.grep_string, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fr', builtin.resume, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
