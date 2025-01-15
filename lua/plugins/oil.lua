e7faf844be171548d73a95fa5ee1ae069cd3b2a7
Richard Weber <git@tebro.simplelogin.com>
2025-01-15 15:02:38 +0200

    Oil winbar

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+		win_options = {
+		    winbar = "%#@attribute.builtin#%{substitute(v:lua.require('oil').get_current_dir(), '^' . $HOME, '~', '')}",
+		}
+	},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
