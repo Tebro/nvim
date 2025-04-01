cbf2b4486b2ba74cfdc5bcbaf25ef3e0ddf28424
Richard Weber <git@tebro.simplelogin.com>
2025-04-01 22:23:03 +0300

    init.lua is not a good root marker

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -3,7 +3,7 @@ return {
 	cmd = {
 		"lua-language-server",
 	},
-	root_markers = { "luarc.json", "luarc.jsonc", ".luacheckrc", ".luacheckrc.json", ".luacheckrc.jsonc", "init.lua", ".git" },
+	root_markers = { "luarc.json", "luarc.jsonc", ".luacheckrc", ".luacheckrc.json", ".luacheckrc.jsonc", ".git" },
 	filetypes = { "lua" },
 	capabilities = capabilities,
 	settings = {
