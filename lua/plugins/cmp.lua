1911425d5f981a5361cfb153d5c966fb4aa11c81
Richard Weber <git@tebro.simplelogin.com>
2025-04-03 18:13:14 +0300

    Improve CMP mapping

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,8 +19,8 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-n'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-					['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					['<C-n'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.close(),
 					['<CR>'] = cmp.mapping.confirm({ select = true }),
