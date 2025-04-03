19212b9473b93ffb1c0e54e22e075ed60dd317a9
Richard Weber <git@tebro.simplelogin.com>
2025-04-03 22:14:02 +0300

    Add a inline diagnostics toggle

---
+++ lua/tebro/functions.lua
@@ -0,0 +1,21 @@
+
+-- Toggle inline diagnostics command
+local inline_diagnostics_enabled = true
+vim.api.nvim_create_user_command('DiagnosticsToggle', function()
+  inline_diagnostics_enabled = not inline_diagnostics_enabled
+  if inline_diagnostics_enabled then
+    -- Enable inline diagnostics
+    vim.diagnostic.config({
+      virtual_text = true,
+    })
+    print("Inline diagnostics enabled")
+  else
+    -- Disable inline diagnostics
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+    print("Inline diagnostics disabled")
+  end
+end, { desc = "Toggle inline diagnostics display" })
+
+
