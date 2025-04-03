49b0991ee78169c8fdeacf2ff2d9ac0d630f2bf0
Richard Weber <git@tebro.simplelogin.com>
2025-04-03 20:35:05 +0300

    Move neoformat to own file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,3 @@
 return {
   "tpope/vim-surround",
-	{
-		"sbdchd/neoformat",
-		config = function ()
-			vim.api.nvim_create_autocmd("BufWritePre",{
-				pattern = { "*.tsx", "*.ts", "*.js", "*.jsx", "*.json", "*.html", "*.css", "*.scss" },
-				command = "Neoformat prettier"
-			})
-			vim.api.nvim_create_autocmd("BufWritePre",{
-				pattern = {"*.go"},
-				command = "Neoformat gofmt"
-			})
-		end
-	},
 }
---
+++ lua/plugins/neoformat.lua
@@ -0,0 +1,13 @@
+return {
+	"sbdchd/neoformat",
+	config = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.tsx", "*.ts", "*.js", "*.jsx", "*.json", "*.html", "*.css", "*.scss" },
+			command = "Neoformat prettier"
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.go" },
+			command = "Neoformat gofmt"
+		})
+	end
+}
