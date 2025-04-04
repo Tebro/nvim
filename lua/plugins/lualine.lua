5dbf0b5771cfa5911c38fc007bcdcd881cde2bce
Richard Weber <git@tebro.simplelogin.com>
2025-04-04 08:53:42 +0300

    Add file path to top of window

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,27 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        extensions = { "lazy",  "fugitive", "mason", "aerial", "oil", "quickfix", "toggleterm" }
-      })
-    end
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				extensions = { "lazy", "fugitive", "mason", "aerial", "oil", "quickfix", "toggleterm" },
+				winbar = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { {'filename', path = 1, file_status = true, shorting_target = 0} },
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {}
+				},
+				inactive_winbar = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { {'filename', path = 1, file_status = true, shorting_target = 0} },
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {}
+				},
+			})
+		end
+	},
 }
