59ea9cbd6021d689fc18cb9fc1c4aaa83dcc6fd3
tebro <git@tebro.simplelogin.com>
2025-04-30 01:03:49 +0300

    Add nixfmt

--- lsp/nil.lua
+++ lsp/nil.lua
@@ -1,7 +1,15 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-return {
-  cmd = { 'nil' },
-	capabilities = capabilities,
-  filetypes = { 'nix' },
-  root_markers = { 'flake.nix', '.git' },
+	return {
+		cmd = { 'nil' },
+		capabilities = capabilities,
+		filetypes = { 'nix' },
+		root_markers = { 'flake.nix', '.git' },
+		settings =  {
+			["nil"] = {
+			formatting = {
+				command = { "nixfmt" }
+			}
+		}
+	},
+
 }
