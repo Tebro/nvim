af93aaa08db25e578cecd92e1aace1ff1644819b
Richard Weber <git@tebro.simplelogin.com>
2025-07-06 14:01:23 +0300

    Telesceope truncate paths

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,12 @@ return { { -- Fuzzy file finder, also does buffers and live grep
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+		require('telescope').setup {
+			defaults = {
+				file_ignore_patterns = { 'node_modules', '.git', '.cache' },
+				path_display = { truncate = 2 },
+			},
+		}
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
