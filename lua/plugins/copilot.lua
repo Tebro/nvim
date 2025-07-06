64b8deb1e02e29ea6b13a73c4601bab94820aa64
Richard Weber <git@tebro.simplelogin.com>
2025-07-06 14:01:46 +0300

    Move to supermaven

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,48 +1,8 @@
 return {
 	{
-		"zbirenbaum/copilot.lua",
-		lazy = false,
-		opts = {
-			suggestion = { enabled = true, auto_trigger = true },
-			panel = { enabled = false },
-		},
-		config = true
-	},
-	{
-		"zbirenbaum/copilot-cmp",
+		"supermaven-inc/supermaven-nvim",
 		config = function()
-			require("copilot_cmp").setup()
-		end
-	},
-	{
-		"olimorris/codecompanion.nvim",
-		opts = {
-			adapters = {
-				copilot = function()
-					return require("codecompanion.adapters").extend("copilot", {
-						schema = {
-							model = {
-								default = "claude-3.7-sonnet"
-							}
-						}
-					})
-				end
-			},
-			strategies = {
-				chat = { adapter = "copilot" },
-				inline = { adapter = "copilot" },
-				cmd = { adapter = "copilot" },
-			}
-		},
-		keys = {
-			{ "<C-a>",      "<cmd>CodeCompanionActions<cr>",     mode = { "n", "v" }, desc = "CodeCompanion Actions popup" },
-			{ "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle CodeCompanion chat" },
-			{ "ga",         "<cmd>CodeCompanionChat Add<cr>",    mode = "v",          desc = "Add selection to CodeCompanion" },
-		},
-
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
+			require("supermaven-nvim").setup({})
+		end,
 	},
 }
