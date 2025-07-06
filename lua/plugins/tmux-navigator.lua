091ac7f89a920262b82f3327c220cd0c753c6dca
Richard Weber <git@tebro.simplelogin.com>
2025-07-06 22:50:48 +0300

    Add tmux-navigator

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateRight",
+			"TmuxNavigateUp",
+			"TmuxNavigateDown",
+			"TmuxNavigatePrevious",
+			"TMuxNavigatorProcessList"
+		},
+		keys = {
+			{ "<C-h>", "<Cmd><C-U>TmuxNavigateLeft<CR>" },
+			{ "<C-j>", "<Cmd><C-U>TmuxNavigateDown<CR>" },
+			{ "<C-k>", "<Cmd><C-U>TmuxNavigateUp<CR>" },
+			{ "<C-l>", "<Cmd><C-U>TmuxNavigateRight<CR>" },
+			{ "<C-\\>", "<Cmd><C-U>TmuxNavigatePrevious<CR>"}
+		}
+	}
+}
