5ab78cdb17d360a174835063bcf14405071f8aad
Richard Weber <git@tebro.simplelogin.com>
2025-08-04 09:44:34 +0300

    Add fff.nvim

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,15 @@
+return {{
+ "dmtrKovalenko/fff.nvim",
+  build = "cargo build --release",
+	--dev = true,
+  opts = {},
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "Toggle FFF",
+    },
+  },
+}}
