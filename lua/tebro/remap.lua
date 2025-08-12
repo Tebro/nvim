15d12afe4fa44dc053a56f6a9522b6f2fd94d0c8
Richard Weber <git@tebro.simplelogin.com>
2025-08-12 10:16:46 +0300

    Improve config shortcuts

--- lua/tebro/remap.lua
+++ lua/tebro/remap.lua
@@ -11,7 +11,7 @@ vim.keymap.set('v', 'fd', '<Esc>')
 vim.keymap.set('n', '<leader>w', vim.cmd.update)
 -- Edit this file
 vim.keymap.set('n', '<leader>fe', ':e $MYVIMRC<cr>')
-vim.keymap.set('n', '<leader>fE', ':e $MYVIMRC<cr>:cd %:h<cr>')
+vim.keymap.set('n', '<leader>fE', ':cd ~/.config/nvim<cr>')
 
 -- Yank to system clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
