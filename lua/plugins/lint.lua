057680d6bdd79d4992ac2e9c5fc0a81b3eecc1e5
Richard Weber <git@tebro.simplelogin.com>
2025-11-13 23:16:05 +0200

    Disable typescript-tools and typescript linting, use LSPs

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,13 +4,13 @@ return {
 		config = function()
 			local lint = require("lint")
 			lint.linters_by_ft = {
-				lua = { "luacheck" },
-				rust = { "cargo" },
+				--lua = { "luacheck" },
+				--rust = { "cargo" },
 				python = { "ruff" },
-				javascript = { "eslint_d" },
-				typescript = { "eslint_d" },
-		 		typescriptreact = { "eslint_d" },
-				javascriptreact = { "eslint_d" },
+				--javascript = { "eslint_d" },
+				--typescript = { "eslint_d" },
+		 		--typescriptreact = { "eslint_d" },
+				--javascriptreact = { "eslint_d" },
 				vue = { "eslint_d" },
 			}
 			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
