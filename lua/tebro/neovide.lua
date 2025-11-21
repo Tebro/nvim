844cf2cb71287e942f3370c04c4c5b864d9544c8
Richard Weber <git@tebro.simplelogin.com>
2025-11-21 10:50:57 +0200

    Disable neovide cursor animation

--- lua/tebro/neovide.lua
+++ lua/tebro/neovide.lua
@@ -2,6 +2,4 @@ vim.g.neovide_scale_factor = 0.7
 vim.g.neovide_cursor_trail_size = 0.2
 vim.g.neovide_cursor_animation_length = 0.1
 vim.g.neovide_cursor_animate_in_insert_mode = false
-vim.g.neovide_cursor_vfx_mode = "railgun"
-vim.g.neovide_cursor_vfx_particle_lifetime = 0.6
-vim.g.neovide_cursor_vfx_particle_phase = 0.6
+vim.g.neovide_cursor_vfx_mode = ""
