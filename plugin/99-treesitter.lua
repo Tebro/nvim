0cd68ed1166d47d35441bfe9d50d53341078095a
Richard Weber <git@tebro.simplelogin.com>
2026-04-05 08:40:32 +0300

    Update treesitter config

--- plugin/99-treesitter.lua
+++ plugin/99-treesitter.lua
@@ -10,20 +10,21 @@ vim.pack.add({
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
-require("nvim-treesitter").setup({
-	ensure_installed = { "c", "lua", "rust", "go", "typescript", "javascript", "html", "haskell", "bash", "json", "make" },
 
-	auto_install = true, -- will install more parsers as you open relevant files
-	highlight = {
-		enable = true,
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true
-	}
-
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { "lua", "rust", "typescript", "javascript", "typescriptreact", "html" },
+  callback = function() vim.treesitter.start() end,
 })
+
+require('nvim-treesitter').install { 
+	"c", 
+	"lua", 
+	"rust",
+	"go",
+	"typescript",
+	"javascript",
+	"html",
+	"bash",
+	"json",
+	"make",
+}
